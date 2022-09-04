75300c41f19be002b2e5fc156418b0a2f717453d
danielgasaniga <daniel.gasaniga@gmail.com>
2022-09-03 22:28:02 -0300

    asd

--- js/products.js
+++ js/products.js
@@ -1,34 +1,104 @@
-let identidades = localStorage.getItem("catID") //Creamos la variable "identidades" que traiga todas las id de las categorias
-const URL = `${PRODUCTS_URL}${identidades}${EXT_TYPE}` //Realizamos la peticion de productos, identitades y salida
-const container = document.getElementById("container") //Creamos la constante "container" en la cual agregamos el contenido
+let identidades = localStorage.getItem("catID"); //Creamos la variable "identidades" que traiga todas las id de las categorias
+const URL = `${PRODUCTS_URL}${identidades}${EXT_TYPE}`; //Realizamos la peticion de productos, identitades y salida
+const container = document.getElementById("container"); //Creamos la constante "container" en la cual agregamos el contenido
+const ascendente = document.getElementById("asc1");
+const descendente = document.getElementById("desc");
+const relevancia = document.getElementById("rel");
+const ordenando = document.getElementById("ordenando");
+var listaProductos = [];
+const ORDER_ASC_BY_COST= "$Asc";
+const ORDER_DESC_BY_COST = "$Desc";
+const ORDER_BY_PROD_REL = "Rel";
 
-function productos(array){ //Dicha funcion recibira un array con sus datos, y los mostrara luego en pantalla
-document.getElementById("container").innerHTML= ""; //Esto vaciara el contenedor
-    for (let productos of array){ // Llamara el array de productos, aqui cambiamos por el de autos
-        //Debajo agregaremos los elementos uno por uno
-        document.getElementById("container").innerHTML += ` 
+function mostrarProductos(array) {
+  //Dicha funcion recibira un array con sus datos, y los mostrara luego en pantalla
+  for (let productos of array) {
+    // Llamara el array de productos, aqui cambiamos por el de productos
+    //Debajo agregaremos los elementos uno por uno
+    document.getElementById("container").innerHTML +=
+      ` 
         
         <div class="list-group-item list-group-item-action">
             <div class="row">
                 <div class="col-3">
-                    <img src="` + productos.image + `" alt="product image" class="img-thumbnail"> 
+                    <img src="` +
+      productos.image +
+      `" alt="product image" class="img-thumbnail"> 
                 </div>
                 <div class="col">
                     <div class="d-flex w-100 justify-content-between"> 
                         <div class="mb-1">
-                        <h4>`+ productos.name + " " + productos.currency + " " + productos.cost + `</h4> 
-                        <p> `+ productos.description +`</p>  
+                        <h4>` +
+      productos.name +
+      " " +
+      productos.currency +
+      " " +
+      productos.cost +
+      `</h4> 
+                        <p> ` +
+      productos.description +
+      `</p>  
                         </div>
-                        <small class="text-muted">` + productos.soldCount  + ` vendidos</small> 
+                        <small class="text-muted">` +
+      productos.soldCount +
+      ` vendidos</small> 
                     </div>
 
                 </div>
             </div>
         </div>
-        `
-    }
+        `;
+  }
 }
 
+document.addEventListener("DOMContentLoaded", function (){
 fetch(URL) //Cuando se resuelve esta "URL", y obtenemos una repuesta, se ejecuta el codigo a continuación
-.then(res=> res.json()) //Entonces cuando la repuesta llega, la funcion flecha sobreescribe la variable
-.then(data =>{productos(data.products)})//Como la repuesta es correcta, colocar la repuesta dentro de productos
+  .then((res) => res.json()) //Entonces cuando la repuesta llega, la funcion flecha sobreescribe la variable
+  .then((data) => {
+    listaProductos = data.products;
+    mostrarProductos(listaProductos);
+  }); //Como la repuesta es correcta, colocar la repuesta dentro de productos
+console.log("putito", document.getElementById("asc1"))
+
+  document.getElementById("asc").addEventListener("change", function(){
+    alert("asdasd")
+    funcAscendente()
+  });
+  
+  document.getElementById("desc").addEventListener("click", function(){
+    mostrarProductos(ORDER_DESC_BY_COST);
+  });
+  
+  document.getElementById("rel").addEventListener("click", function(){
+    mostrarProductos(ORDER_BY_PROD_REL);
+  });
+})  
+
+function funcAscendente (){
+   alert("dasdasd")
+   listaProductos.sort((a, b) => {
+       if(a.cost > b.cost) {return -1;}
+       if(a.cost < b.cost) {return 1;}
+       return 0;
+   });
+   mostrarProductos(listaProductos)
+ };
+
+descendente.addEventListener("click", function(){
+  listaProductos.sort((a, b) => {
+      if(a.cost < b.cost) {return -1;}
+      if(a.cost > b.cost) {return 1;}
+      return 0;
+  });
+  mostrarProductos(listaProductos)
+});
+
+relevancia.addEventListener("click", function(){
+  listaProductos.sort((a, b) => {
+      if(a.soldCount > b.soldCount) {return -1;}
+      if(a.soldCount < b.soldCount) {return 1;}
+      return 0;
+  });
+  mostrarProductos(listaProductos)
+});
+
