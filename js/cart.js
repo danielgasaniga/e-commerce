d0ecd5b855a15aa86986827e4be29119dbb61f81
danielgasaniga <daniel.gasaniga@gmail.com>
2022-10-16 21:45:58 -0300

    fin entrega 5

--- js/cart.js
+++ js/cart.js
@@ -2,7 +2,7 @@ const usuarioId = 25801; //Creo una constante con su id correspondiente
 const contenedor = document.getElementById("cartContainer"); //Creo una constante trayendo el contenedor del carro
 
 document.addEventListener("DOMContentLoaded", function () { //Realizo un addevenlistener para que me cargue el DOM
-  let URL_cart = CART_INFO_URL + usuarioId + EXT_TYPE; ////Realizamos la peticion de productos, identitades y salida
+  let URL_cart = CART_INFO_URL + usuarioId + EXT_TYPE; ////Realizo la peticion de productos, identitades y salida, y que me ejecute la funcion peticion, que se encuentra debajo
   fetch(URL_cart)
     .then(function (response) {
       return response.json();
@@ -12,7 +12,7 @@ document.addEventListener("DOMContentLoaded", function () { //Realizo un addeven
     });
 });
 
-function peticion(data) {
+function peticion(data) { //Realizo una funcion con una tabla en las cuales ingreso los datos solicitados
   let htmlContentToAppend = "";
   for (const item of data.articles) {
     htmlContentToAppend += `<table class="table table"> <br>
@@ -29,19 +29,18 @@ function peticion(data) {
         <tr>
           <td><img style="width: 65px; height: 40px;"  class="img-fluid" src="${item.image}"></td>
           <td>${item.name}</td>
-          <td>${item.currency}<span id="preciofijo"> ${item.unitCost} </span></td>
-          <td><input id="variableCantidades" type="number" style="width:70px"></td>
-          <td id = "subTotal"><strong>${item.currency} </strong></td>
-        </tr>`;
+          <td>${item.currency} ${item.unitCost}</td>
+          <td><input id="variableCantidades" oninput="subTotal(${item.unitCost})" type="number" style="width:70px"></td>
+          <td><b>${item.currency} <span id="resultado"${item.unitCost}></b></td>
+        </tr>
+        </table>`;
+      }
     contenedor.innerHTML = htmlContentToAppend;
-
-    let variableCantidad = document.getElementById("variableCantidades")
-    let subTotal = document.getElementById("subTotal")
-    let preciofijo = document.getElementById("preciofijo")
-
- variableCantidad.addEventListener("keyup", function () {
-      var result = item.unitCost * variableCantidad.value;
-      subTotal.innerHTML = `<strong>${item.currency} ${result}</strong>`;
-    });
   }
-}
+
+
+  function subTotal(total){ //Creo una funcion para realziar la multiplicacion, para luego mostrarla en el subtotal
+    let variableCantidades = document.getElementById("variableCantidades").value
+    resultado = total * variableCantidades
+    return (document.getElementById("resultado").innerHTML = resultado) 
+  }
