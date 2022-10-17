b194fdaa7944d4f17928496e7e28f4ab911e5b5c
danielgasaniga <daniel.gasaniga@gmail.com>
2022-10-16 22:40:16 -0300

    fin

--- js/cart.js
+++ js/cart.js
@@ -1,7 +1,8 @@
 const usuarioId = 25801; //Creo una constante con su id correspondiente
 const contenedor = document.getElementById("cartContainer"); //Creo una constante trayendo el contenedor del carro
 
-document.addEventListener("DOMContentLoaded", function () { //Realizo un addevenlistener para que me cargue el DOM
+document.addEventListener("DOMContentLoaded", function () {
+  //Realizo un addevenlistener para que me cargue el DOM
   let URL_cart = CART_INFO_URL + usuarioId + EXT_TYPE; ////Realizo la peticion de productos, identitades y salida, y que me ejecute la funcion peticion, que se encuentra debajo
   fetch(URL_cart)
     .then(function (response) {
@@ -12,7 +13,8 @@ document.addEventListener("DOMContentLoaded", function () { //Realizo un addeven
     });
 });
 
-function peticion(data) { //Realizo una funcion con una tabla en las cuales ingreso los datos solicitados
+function peticion(data) {
+  //Realizo una funcion con una tabla en las cuales ingreso los datos solicitados
   let htmlContentToAppend = "";
   for (const item of data.articles) {
     htmlContentToAppend += `<table class="table table"> <br>
@@ -22,7 +24,7 @@ function peticion(data) { //Realizo una funcion con una tabla en las cuales ingr
           <th scope="col">Nombre</th>
           <th scope="col">Costo</th>
           <th scope="col">Cantidad</th>
-          <th scope="col" >SubTotal</th>
+          <th scope="col">SubTotal</th>
         </tr>
       </thead>
       <tbody>
@@ -34,13 +36,13 @@ function peticion(data) { //Realizo una funcion con una tabla en las cuales ingr
           <td><b>${item.currency} <span id="resultado"${item.unitCost}></b></td>
         </tr>
         </table>`;
-      }
-    contenedor.innerHTML = htmlContentToAppend;
   }
+  contenedor.innerHTML = htmlContentToAppend;
+}
 
-
-  function subTotal(total){ //Creo una funcion para realziar la multiplicacion, para luego mostrarla en el subtotal
-    let variableCantidades = document.getElementById("variableCantidades").value
-    resultado = total * variableCantidades
-    return (document.getElementById("resultado").innerHTML = resultado) 
-  }
+function subTotal(total) {
+  //Creo una funcion para realziar la multiplicacion, para luego mostrarla en el subtotal
+  let variableCantidades = document.getElementById("variableCantidades").value;
+  resultado = total * variableCantidades;
+  return (document.getElementById("resultado").innerHTML = resultado);
+}
