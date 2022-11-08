8f8eec99bcd383e8778a6d51cdb8e04cb20fd81d
danielgasaniga <daniel.gasaniga@gmail.com>
2022-11-07 21:33:50 -0300

    entrega 6

--- js/cart.js
+++ js/cart.js
@@ -1,54 +1,150 @@
-const usuarioId = 25801; //Creo una constante con su id correspondiente
-const contenedor = document.getElementById("cartContainer"); //Creo una constante trayendo el contenedor del carro
+const info_carrito = "https://japceibal.github.io/emercado-api/user_cart/25801.json"
 
-document.addEventListener("DOMContentLoaded", function () {
-  //Realizo un addevenlistener para que me cargue el DOM
-  let URL_cart = CART_INFO_URL + usuarioId + EXT_TYPE; ////Realizo la peticion de productos, identitades y salida, y que me ejecute la funcion peticion, que se encuentra debajo
-  fetch(URL_cart)
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (item) {
-      peticion(item);
-    });
-});
+// getElementById necesarios
+const lista = document.getElementById("selector")
+const tarjeta = document.getElementById("flexRadioDefault1")
+const transferencia = document.getElementById("flexRadioDefault2")
+const numero_cuenta = document.getElementById("numero_cuenta")
+const numero_tarjeta = document.getElementById("numero_tarjeta")
+const codigo_seguridad = document.getElementById("codigo_seguridad")
+const vencimiento_tarjeta = document.getElementById("vencimiento_tarjeta")
+const boton_guardar = document.getElementById("boton_guardar")
 
-function peticion(data) {
-  //Realizo una funcion con una tabla en las cuales ingreso los datos solicitados
-  let htmlContentToAppend = "";
-  for (const item of data.articles) {
-    htmlContentToAppend += `<table class="table table"> <br>
-      <thead>
-        <tr>
-          <th scope="col"></th>
-          <th scope="col">Nombre</th>
-          <th scope="col">Costo</th>
-          <th scope="col">Cantidad</th>
-          <th scope="col">SubTotal</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td><img style="width: 65px; height: 40px;"  class="img-fluid" src="${item.image}"></td>
-          <td>${item.name}</td>
-          <td>${item.currency} ${item.unitCost}</td>
-          <td><input id="variableCantidades" oninput="subTotal(${item.unitCost})" type="number" style="width:70px"></td>
-          <td><b>${item.currency} <span id="resultado"${item.unitCost}></b></td>
-        </tr>
-        </table>`;
+const tabla = document.getElementById("tabla")
+const costos = document.getElementById("costos")
+document.addEventListener('DOMContentLoaded', async function () {
+  let dato = await getJSONData(info_carrito)
+
+  tabla.innerHTML = ""
+  tabla.innerHTML += `
+  <thead>
+  <tr>
+    <th scope="col"></th>
+    <th scope="col">Nombre</th>
+    <th scope="col">Costo</th>
+    <th scope="col">Cantidad</th>
+    <th scope="col">Subtotal</th>
+  </tr>
+</thead>`
+  for (let articulos of dato.data.articles) {
+    tabla.innerHTML += `
+ <tbody>
+  <tr>
+    <th scope="row"><img style="width: 100px;" src="${articulos.image}" class="img-thumbnail"> </th>
+    <td>${articulos.name}</td>
+    <td>${articulos.currency} ${articulos.unitCost}</td>
+    <td>
+    <input oninput="calcular_costo(${articulos.unitCost}, this.value)" type="number" value="${articulos.count}" min="1" max="5" id="input"></td>
+    <td><b>${articulos.currency}<span id="subtotal">${articulos.unitCost}</b> </span> </td>
+  </tr>
+  `
+
+    costos.innerHTML += `
+   <ul class="list-group">
+  <li class="list-group-item d-flex justify-content-between align-items-center">
+    Subtotal del producto (U$D)
+    <span class="badge bg-primary rounded-pill" id="costoss">${articulos.unitCost}</span>
+  </li>
+  <li class="list-group-item d-flex justify-content-between align-items-center">
+    Costo del envio (U$D)
+    <span class="badge bg-primary rounded-pill" id="envios"></span>
+  </li>
+  <li class="list-group-item d-flex justify-content-between align-items-center">
+    Total
+    <span class="badge bg-primary rounded-pill" id="total"></span>
+  </li>
+</ul>
+   `
   }
-  contenedor.innerHTML = htmlContentToAppend;
+ 
+  const envio = document.getElementById("envios")
+
+  function calcular_envio() {
+    let subtotal = Number(document.getElementById("subtotal").textContent)
+    let indice = lista.selectedIndex
+    if (indice == 1)
+      envio.innerHTML = subtotal * 0.15
+    else if (indice == 2)
+      envio.innerHTML = subtotal * 0.07
+    else if (indice == 3)
+      envio.innerHTML = subtotal * 0.05
+  }
+
+
+  lista.addEventListener("click", () => {
+    calcular_envio();
+    sumar_total()
+  })
+
+  function sumar_total() {
+    let subtotal = Number(document.getElementById("subtotal").textContent)
+    const total = document.getElementById("total")
+    total.innerHTML = subtotal + Number(envio.textContent)
+  }
+
+
+  
+
+})
+
+
+function calcular_costo(parametro1, parametro2) {
+  let costoss = document.getElementById("costoss")
+  let subtotal = document.getElementById("subtotal");
+  subtotal.innerHTML = parametro1 * parametro2
+  costoss.innerHTML = parametro1 * parametro2
 }
 
-function subTotal(total) {
-  //Creo una funcion para realziar la multiplicacion, para luego mostrarla en el subtotal
-  let variableCantidades = document.getElementById("variableCantidades").value;
-  resultado = total * variableCantidades;
-  return (document.getElementById("resultado").innerHTML = resultado);
+
+
+function chequear() {
+  if (tarjeta.checked) {
+    transferencia.disabled = true
+    numero_cuenta.disabled = true
+  } else if (transferencia.checked) {
+    tarjeta.disabled = true
+    numero_tarjeta.disabled = true
+    codigo_seguridad.disabled = true
+    vencimiento_tarjeta.disabled = true
+  }
 }
 
-const premium = document.getElementById("envpremium");
-const express = document.getElementById("envexpress");
-const standard = document.getElementById("envstandard");
-const finalizar = document.getElementById("botonFinalizar")
+const calle = document.getElementById("calle")
+const numero = document.getElementById("numero")
+const esquina = document.getElementById("esquina")
+const boton_comprar = document.getElementById("boton_comprar")
 
+function validad (){
+  indice = lista.selectedIndex;
+  if( indice == null || indice == 0 ) {
+    return false;
+  }
+boton_comprar.addEventListener ("click", () => {
+validad()
+alert ("compra realizada con exito")
+})}
+
+
+
+(function () {
+  'use strict'
+  // Fetch all the forms we want to apply custom Bootstrap validation styles to
+  let forms = document.querySelectorAll('.needs-validation')
+  // Loop over them and prevent submission
+  Array.prototype.slice.call(forms)
+    .forEach(function (form) {
+      form.addEventListener('submit', function (event) {
+       
+        if (!form.checkValidity()) {
+         {
+          event.preventDefault()
+          event.stopPropagation()
+          }
+          
+        } 
+
+
+        form.classList.add('was-validated')
+      }, false)
+    })
+})()
