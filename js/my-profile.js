c60b32074dd5a6ef33f4220177b58f565fd7f22f
danielgasaniga <daniel.gasaniga@gmail.com>
2022-11-10 15:52:16 -0300

    html terminado entrega 7

--- js/my-profile.js
+++ js/my-profile.js
@@ -6,3 +6,36 @@ const email = document.getElementById("email");
 const telefonoDeContacto = document.getElementById("telefonoDeContacto");
 const boton_guardar = document.getElementById("boton_guardar");
 
+
+function validad() {
+    indice = lista.selectedIndex;
+    if (indice == null || indice == 0) {
+      return false;
+    }
+    boton_guardar.addEventListener("click", () => {
+      validad();
+    });
+  }
+
+(function () {
+    "use strict";
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    let forms = document.querySelectorAll(".needs-validation");
+    // Loop over them and prevent submission
+    Array.prototype.slice.call(forms).forEach(function (form) {
+      form.addEventListener(
+        "submit",
+        function (event) {
+          event.preventDefault();
+          if (!form.checkValidity()) {
+              event.stopPropagation();
+          }else{
+            showAlertSuccess()
+          }
+  
+          form.classList.add("was-validated");
+        },
+        false
+      );
+    });
+  })();
