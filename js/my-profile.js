7010505384a70021fc8aaeda73131bea39d3f739
danielgasaniga <daniel.gasaniga@gmail.com>
2022-11-10 16:07:25 -0300

    a

--- js/my-profile.js
+++ js/my-profile.js
@@ -5,6 +5,8 @@ const segundoApellido = document.getElementById("segundoApellido");
 const email = document.getElementById("email");
 const telefonoDeContacto = document.getElementById("telefonoDeContacto");
 const boton_guardar = document.getElementById("boton_guardar");
+//EXPRESION REGULAR PARA CORROBORAR FORMATO DE CORREO
+var emailRegex = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i
 
 
 function validad() {
@@ -39,3 +41,24 @@ function validad() {
       );
     });
   })();
+
+
+  //MENSAJES DE ERROR Y CONFIRMACIÓN
+function showAlertSuccess() {
+  document.getElementById("alert-success").classList.add("show");
+}
+
+function showAlertError() {
+  document.getElementById("alert-danger").classList.add("show");
+}
+
+//FUNCIONES COMPLEMENTARIAS DE 'ENVIAR REGISTRO'
+
+function verificarCampos() {
+  return primerNombre.value.trim().length > 1 && primerApellido.value.trim().length > 1 && emailRegex.test(email.value);
+}
+
+//FUNCIÓN PRINCIPAL A LA CUAL APUNTA CART.HTML
+function guardarRegistro() {
+  return (verificarCampos && check.checked) ? showAlertSuccess() : showAlertError();
+}
