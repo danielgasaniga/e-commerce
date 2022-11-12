ab52f64e1318701ddef24e3c00662e8199e45de4
danielgasaniga <daniel.gasaniga@gmail.com>
2022-11-12 15:31:25 -0300

    Faltan detalles de la entrega 7

--- js/my-profile.js
+++ js/my-profile.js
@@ -4,54 +4,31 @@ const primerApellido = document.getElementById("primerApellido");
 const segundoApellido = document.getElementById("segundoApellido");
 const email = document.getElementById("email");
 const telefonoDeContacto = document.getElementById("telefonoDeContacto");
-const boton_guardar = document.getElementById("boton_guardar");
-//EXPRESION REGULAR PARA CORROBORAR FORMATO DE CORREO
-var emailRegex = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i
 
-(function () {
-    "use strict";
-    // Fetch all the forms we want to apply custom Bootstrap validation styles to
-    let forms = document.querySelectorAll(".needs-validation");
-    // Loop over them and prevent submission
-    Array.prototype.slice.call(forms).forEach(function (form) {
-      form.addEventListener(
-        "submit",
-        function (event) {
-          event.preventDefault();
-          if (!form.checkValidity()) {
-              event.stopPropagation();
-          }else{
-            showAlertSuccess()
-          }
-  
-          form.classList.add("was-validated");
-        },
-        false
-      );
-    });
-  })();
+primerNombre.value = localStorage.getItem("primerNombre")
+segundoNombre.value = localStorage.getItem("segundoNombre")
+primerApellido.value = localStorage.getItem("primerApellido")
+segundoApellido.value = localStorage.getItem("segundoApellido")
+email.value = localStorage.getItem("logueado")
+telefonoDeContacto.value = localStorage.getItem("telefonoDeContacto")
+
+const forms = document.querySelectorAll('.needs-validation')
 
 
-  //MENSAJES DE ERROR Y CONFIRMACIÓN
-function showAlertSuccess() {
-  document.getElementById("alert-success").classList.add("show");
-}
+forms.forEach(form => {
+  form.addEventListener('submit', event => {
+    event.preventDefault()
+    localStorage.setItem("primerNombre", primerNombre.value)
+    localStorage.setItem("segundoNombre", segundoNombre.value)
+    localStorage.setItem("primerApellido", primerApellido.value)
+    localStorage.setItem("segundoApellido", segundoApellido.value)
+    localStorage.setItem("telefonoDeContacto", telefonoDeContacto.value)
 
-function showAlertError() {
-  document.getElementById("alert-danger").classList.add("show");
-}
+    if (!form.checkValidity()) {
+      event.stopPropagation()
+      
+    }
 
-//FUNCIONES COMPLEMENTARIAS DE 'ENVIAR REGISTRO'
-
-function verificarCampos() {
-  return primerNombre.value.trim().length > 1 && primerApellido.value.trim().length > 1 && emailRegex.test(email.value);
-}
-
-//FUNCIÓN PRINCIPAL A LA CUAL APUNTA CART.HTML
-function guardarRegistro() {
-  return (verificarCampos && check.checked) ? showAlertSuccess() : showAlertError();
-}
-
-boton_guardar.addEventListener("click", () => {
-  guardarRegistro();
-});
+    form.classList.add('was-validated')
+  }, false)
+})
