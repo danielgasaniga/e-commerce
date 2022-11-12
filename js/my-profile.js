9d8d297363c7bf71decf7aa06b0119f378c24a81
danielgasaniga <daniel.gasaniga@gmail.com>
2022-11-12 15:35:32 -0300

    Falta solo el correo

--- js/my-profile.js
+++ js/my-profile.js
@@ -5,30 +5,32 @@ const segundoApellido = document.getElementById("segundoApellido");
 const email = document.getElementById("email");
 const telefonoDeContacto = document.getElementById("telefonoDeContacto");
 
-primerNombre.value = localStorage.getItem("primerNombre")
-segundoNombre.value = localStorage.getItem("segundoNombre")
-primerApellido.value = localStorage.getItem("primerApellido")
-segundoApellido.value = localStorage.getItem("segundoApellido")
-email.value = localStorage.getItem("logueado")
-telefonoDeContacto.value = localStorage.getItem("telefonoDeContacto")
+primerNombre.value = localStorage.getItem("primerNombre");
+segundoNombre.value = localStorage.getItem("segundoNombre");
+primerApellido.value = localStorage.getItem("primerApellido");
+segundoApellido.value = localStorage.getItem("segundoApellido");
+email.value = localStorage.getItem("logueado");
+telefonoDeContacto.value = localStorage.getItem("telefonoDeContacto");
 
-const forms = document.querySelectorAll('.needs-validation')
+const forms = document.querySelectorAll(".needs-validation");
 
+forms.forEach((form) => {
+  form.addEventListener(
+    "submit",
+    (event) => {
+      event.preventDefault();
+      localStorage.setItem("primerNombre", primerNombre.value);
+      localStorage.setItem("segundoNombre", segundoNombre.value);
+      localStorage.setItem("primerApellido", primerApellido.value);
+      localStorage.setItem("segundoApellido", segundoApellido.value);
+      localStorage.setItem("telefonoDeContacto", telefonoDeContacto.value);
 
-forms.forEach(form => {
-  form.addEventListener('submit', event => {
-    event.preventDefault()
-    localStorage.setItem("primerNombre", primerNombre.value)
-    localStorage.setItem("segundoNombre", segundoNombre.value)
-    localStorage.setItem("primerApellido", primerApellido.value)
-    localStorage.setItem("segundoApellido", segundoApellido.value)
-    localStorage.setItem("telefonoDeContacto", telefonoDeContacto.value)
+      if (!form.checkValidity()) {
+        event.stopPropagation();
+      }
 
-    if (!form.checkValidity()) {
-      event.stopPropagation()
-      
-    }
-
-    form.classList.add('was-validated')
-  }, false)
-})
+      form.classList.add("was-validated");
+    },
+    false
+  );
+});
